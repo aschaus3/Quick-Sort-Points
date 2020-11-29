8f041d645d91ed09928e9bcdd43566381951d78f
Andrew Schaus <andrewschaus3@gmail.com>
2020-11-29 14:29:58 -0500

    Made the Quick Sort class

---
+++ src/com/company/QuickSort.java
@@ -0,0 +1,38 @@
+package com.company;
+import java.util.Comparator;
+import java.util.LinkedList;
+public class QuickSort
+{
+    public static <K> void quickSort(LinkedList<K> s, Comparator<K> comp)
+    {
+        int n = s.size();   //Getting the size
+        if(n < 2) return;   //Base case
+
+        K pivot = s.getFirst();
+        LinkedList<K> L = new LinkedList<>();
+        LinkedList<K> E = new LinkedList<>();
+        LinkedList<K> G = new LinkedList<>();
+
+        while(!s.isEmpty())
+        {
+            K element = s.remove();
+            int c = comp.compare(element,pivot);
+            if(c < 0)
+                L.add(element);
+            else if(c == 0)
+                E.add(element);
+            else
+                G.add(element);
+        }
+
+        quickSort(L,comp);
+        quickSort(G,comp);
+
+        while(!L.isEmpty())
+            s.add(L.remove());
+        while(!E.isEmpty())
+            s.add(E.remove());
+        while(!G.isEmpty())
+            s.add(G.remove());
+    }
+}
